33d89c2566523487a2e440a5c5a7677e1e116017
Alexander <alkoclick@gmail.com>
2017-11-10 20:33:46 +0200

    Create initial POJOs

---
+++ src/main/java/model/Mentor.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Mentor {
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String description;
+}
---
+++ src/main/java/model/Opinion.java
@@ -0,0 +1,6 @@
+package model;
+
+public class Opinion {
+	private String name;
+	private String opinion;
+}
