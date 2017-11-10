e392edfbd84d0866f4f97a2f96dc59490d1bf225
Alexander <alkoclick@gmail.com>
2017-11-10 20:48:40 +0200

    Add hibernate to gradle buildfile, POJO accessors

--- src/main/java/model/Mentor.java
+++ src/main/java/model/Mentor.java
@@ -1,8 +1,34 @@
 package model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class Mentor {
+	private long id;
 	private String firstName;
 	private String lastName;
 	private String email;
 	private String description;
+
+	@Id
+	@GeneratedValue
+	public long getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getDescription() {
+		return description;
+	}
 }
--- src/main/java/model/Opinion.java
+++ src/main/java/model/Opinion.java
@@ -1,6 +1,24 @@
 package model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class Opinion {
+	private long id;
 	private String name;
 	private String opinion;
+
+	@Id
+	@GeneratedValue
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getOpinion() {
+		return opinion;
+	}
 }
